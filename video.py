3f3a16c98c2ba46b314cb1ae25fac1f14a9267ad
Namenaro <nanenaro@gmail.com>
2017-02-01 13:18:13 +0300

    erteye

--- video.py
+++ video.py
@@ -1,23 +1,39 @@
 import cv2
 import os
 
-print(cv2.__version__)
+
+def print_video_info(videoName):
+    if os.path.isfile(name):
+        size = os.path.getsize(name)
+        print 'video size is ' + str(size) + ' bytes'
+    else:
+        print 'file doesn\'t exist'
+        return
+
+def show_video_grayscale(videoName):
+    if not os.path.isfile(name):
+        print 'file doesn\'t exist'
+        return
+
+    cap = cv2.VideoCapture(name)
+
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret != True:
+            break
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        cv2.imshow('frame',gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+###
 name = 'sample.avi'
+print 'opencv version: ' + cv2.__version__
 
-if (os.path.isfile (name)):
-   size= os.path.getsize(name)
-
-cap = cv2.VideoCapture(name)
-
-while(cap.isOpened()):
-   ret, frame = cap.read()
-   if ret != True:
-       break
-   gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-   cv2.imshow('frame',gray)
-   if cv2.waitKey(1) & 0xFF == ord('q'):
-       break
-
-cap.release()
-cv2.destroyAllWindows()
+print_video_info (name)
+show_video_grayscale(name)
