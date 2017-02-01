7178fc965c9af262ec7e4a19c55102a306844f66
Namenaro <nanenaro@gmail.com>
2017-02-01 17:17:17 +0300

    networkx

--- video.py
+++ video.py
@@ -1,7 +1,9 @@
 import cv2
 import os
+import networkx as nx
+import matplotlib.pyplot as plt
 
-#89797987987978
+# VIDEO
 def print_video_info(videoName):
     if os.path.isfile(name):
         size = os.path.getsize(name)
@@ -31,9 +33,27 @@ def show_video_grayscale(videoName):
     cv2.destroyAllWindows()
 
 
-###
+# GRAPHS
+
+def sample_graph_networkx():
+    G = nx.Graph()
+    G.add_node('1')
+    G.add_node('2')
+    G.add_node('3')
+    G.add_node('4')
+    G.add_node('5')
+    G.add_edge('1', '2')
+    G.add_edge('2', '3')
+    G.add_edge('3', '4')
+    G.add_edge('4', '1')
+    G.add_edge('4', '5')
+    nx.draw_spectral(G)
+    plt.show()
+
+### CALL FUNCTIONS
 name = 'sample.avi'
 print 'opencv version: ' + cv2.__version__
 
-print_video_info (name)
-show_video_grayscale(name)
+#print_video_info (name)
+#show_video_grayscale(name)
+sample_graph_networkx()
