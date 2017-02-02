4dc7ff68ab9332f9ecfffcbebdd68b72c91d927a
Namenaro <nanenaro@gmail.com>
2017-02-02 16:02:37 +0300

    diff frames video

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import cv2
 import os
+import networkx as nx
+import matplotlib.pyplot as plt
 
 def print_video_info(videoName):
     if os.path.isfile(videoName):
@@ -11,7 +13,7 @@ def print_video_info(videoName):
 
 
 
-def show_video_grayscale(videoName):
+def show_video_gray(videoName):
     if not os.path.isfile(videoName):
         print 'file doesn\'t exist'
         return
@@ -30,3 +32,72 @@ def show_video_grayscale(videoName):
 
     cap.release()
     cv2.destroyAllWindows()
+
+def show_video_gray_diff(videoName):
+    if not os.path.isfile(videoName):
+        print 'file doesn\'t exist'
+        return
+
+    cap = cv2.VideoCapture(videoName)
+    prevFrame = None
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret != True:
+            break
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        if prevFrame is None:
+            prevFrame = frame
+            continue
+        else:
+           diff = pixelwise_diff(prevFrame, frame)
+           prevFrame = frame
+
+        cv2.imshow('diff', diff)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+def show_first_n_frames(videoName, n):
+    if not os.path.isfile(videoName):
+        print 'file doesn\'t exist'
+        return
+
+    cap = cv2.VideoCapture(videoName)
+    i=0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret != True:
+            break
+        cv2.imshow('frame'+str(i), frame)
+        if i > n-1 :
+            break
+        i += 1
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    while True:
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cap.release()
+            cv2.destroyAllWindows()
+            break
+
+def pixelwise_diff(prev, next):
+    diff = prev - next
+    return diff
+
+def sample_graph_networkx():
+    G = nx.Graph()
+    G.add_node('1')
+    G.add_node('2')
+    G.add_node('3')
+    G.add_node('4')
+    G.add_node('5')
+    G.add_edge('1', '2')
+    G.add_edge('2', '3')
+    G.add_edge('3', '4')
+    G.add_edge('4', '1')
+    G.add_edge('4', '5')
+    nx.draw_spectral(G)
+    plt.show()
--- video.py
+++ video.py
@@ -5,25 +5,7 @@ import matplotlib.pyplot as plt
 import utils
 
 
-def sample_graph_networkx():
-    G = nx.Graph()
-    G.add_node('1')
-    G.add_node('2')
-    G.add_node('3')
-    G.add_node('4')
-    G.add_node('5')
-    G.add_edge('1', '2')
-    G.add_edge('2', '3')
-    G.add_edge('3', '4')
-    G.add_edge('4', '1')
-    G.add_edge('4', '5')
-    nx.draw_spectral(G)
-    plt.show()
-
-### CALL FUNCTIONS
-name = 'sample.avi'
+name = 'bigvideo.mp4'
 print 'opencv version: ' + cv2.__version__
 
-utils.print_video_info (name)
-utils.show_video_grayscale(name)
-sample_graph_networkx()
+utils.show_video_gray_diff(name)
