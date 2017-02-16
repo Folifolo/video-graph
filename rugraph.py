91c7becf3a5b2011955d0859409ee9939be00c67
Namenaro <nanenaro@gmail.com>
2017-02-16 13:44:33 +0300

    Работа с нейронами событий: продолжение

--- rugraph.py
+++ rugraph.py
@@ -15,13 +15,19 @@ SALIENCY_FADING_PER_TIME = 0.1
 # может зарегстрировать в свое рец. поле новодобавлемый NS-нейрон
 EVENT_KERNEL_CAPACITY = 6
 
+class GraphError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
 class RuGraph:
     def __init__(self):
         self.G = nx.DiGraph()
         self.generator = itertools.count(0)
         self.max_layer = 0
 
-    def add_input_layer (self, shape):
+    def _add_input_layer (self, shape):
         for i in range(shape[0]):
             for j in range(shape[1]):
                 self._add_input_neuron((i,j))
@@ -31,7 +37,6 @@ class RuGraph:
         self.G.add_node(n,
                         type = "S",
                         layer = 0,
-                        useful = 1,
                         activation = 0,
                         index = index
                         )
@@ -40,27 +45,37 @@ class RuGraph:
         for i in range(input_signal.shape[0]):
             for j in range(input_signal.shape[1]):
                 n = filter(lambda (n, d): d['index'] == (i,j), self.G.nodes(data=True))
-                id = n[0][0]
+                id = n[0][0] # фильтр всегда находит один нейрон с такими координатами
                 self.G.node[id]['activation'] = input_signal[i,j]
 
-    def forvard_pass(self, input_siganl):
-        elf._pass_to_init_layer(input_siganl)
-
-
-    def add_event_neuron (self, source_ids, source_attrs):
+    def _add_event_neuron (self, source_ids, source_attrs):
         new_id = self.generator.next()
-        #TODO !! leayer = max(layers of source) + 1
         self.G.add_node(new_id,
                         type="N",
-                        layer=0,
-                        usefulness=1,
-                        activation=0,
+                        layer= self._get_layer_num_for_neuron(source_attrs),
+                        activation=1
                         )
         for id in source_ids:
-            self.G.add_edge(id, new_id, weight = source_attrs[id]['activation'])
+            self.G.add_edge(id, new_id, weight=source_attrs[id]['activation'])
+
+    def pass_to_neuron(self, id):
+        sources = self.G.predecessors(id)
+        if len(sources) == 0:
+            raise GraphError("Neuron has no input connections")
+        print 'uuuuuu'
+
+    def _get_layer_num_for_neuron(self,  source_attrs):
+        # TODO !! leayer = max(layers of source) + 1
+        pass
+
+    def forward_pass(self, input_siganl):
+        self._pass_to_init_layer(input_siganl)
+
+
+
 
     def delete_neuron(self, id):
-        pass
+        self.G.remove_node(id)
 
 class RuGraphVisualizer:
     def __init__(self):
@@ -85,8 +100,8 @@ print "--------test-------"
 M = utils.generate_sparse_matrix(15,15)
 
 graph = RuGraph()
-graph.add_input_layer( M.A.shape )
-graph.forvard_pass(M.A)
+graph._add_input_layer( M.A.shape )
+graph.forward_pass(M.A)
 vis = RuGraphVisualizer()
 vis.draw_input_layer_with_act(graph.G)
 
