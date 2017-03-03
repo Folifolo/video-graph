ba632f1e1d35af909647cf57f3bed42165289d0a
Namenaro <nanenaro@gmail.com>
2017-03-03 15:19:45 +0300

    прикрепления графаа к видео

--- rugaze.py
+++ rugaze.py
@@ -1,22 +1,25 @@
 # -*- coding: utf-8 -*
 import os
 import cv2
-import utils
+import numpy as np
 
 THRESHOLD_FOR_DIFF = 10
 
 class SimpleVideoGaze:
-    def __init__(self, videoname, side, center=None, show=False, print_it=True):
+    def __init__(self, videoname, side, left_top_coord=None, show=False, print_it=True):
         assert side != 0, "gaze square is zero"
         self.videoname = videoname
         if not os.path.isfile(self.videoname):
             print 'file doesn\'t exist'
             return
-        self.center = center
+        self. left_top_coord =  left_top_coord
         self.capture = cv2.VideoCapture(self.videoname)
         self.prev_frame = None
         self.show = show
         self.print_it = print_it
+        if print_it: #весьма замедляет всё, но зато большую матрицу всю видно
+            np.set_printoptions(threshold=np.inf)
+        self.side = side
         assert self.capture.isOpened(), "video file corrupted?"
         ret, frame = self.capture.read()
         if ret is True:
@@ -24,8 +27,9 @@ class SimpleVideoGaze:
         else:
             print 'one-frame video?'
 
+
     def get_shape(self):
-        return self.prev_frame.shape
+        return (self.side, self.side)
 
     def get_next_fixation(self):
         if self.prev_frame is None:
@@ -46,12 +50,18 @@ class SimpleVideoGaze:
                 return None
         if self.print_it:
             print subframe
+            print "---"
         self.prev_frame = frame
 
         return subframe
 
     def get_subframe(self, frame1, frame2):
-        return frame2 - frame1
+        diff = frame2 - frame1
+        X1 = self.left_top_coord[0]
+        X2 = self.left_top_coord[0] + self.side
+        Y1 = self.left_top_coord[1]
+        Y2 = self.left_top_coord[1] + self.side
+        return diff[X1:X2+1, Y1:Y2+1]
 
     def show_video(self):
         while self.capture.isOpened():
@@ -73,13 +83,15 @@ class SimpleVideoGaze:
         self.capture.release()
         cv2.destroyAllWindows()
 
+
 class GazeTest:
     def __init__(self):
         pass
+
     def test(self):
         print "gaze-test"
         video = 'bigvideo.mp4'
-        input = SimpleVideoGaze(videoname=video, print_it=True,show=True, side=10)
+        input = SimpleVideoGaze(videoname=video, print_it=True, show=True, side=200, left_top_coord=(220,220))
         print input.get_shape()
         while True:
             img = input.get_next_fixation()
@@ -88,6 +100,4 @@ class GazeTest:
 
 gaze = GazeTest()
 gaze.test()
-input = SimpleVideoGaze(videoname='bigvideo.mp4', print_it=True, show=True, side=10)
-print input.get_shape()
-#input.show_video()
+
